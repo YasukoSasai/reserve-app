c36173e74c144ac4f604e8eb439cbdb7ea5e26e5
Yasuko Sasai <>
2021-08-16 08:32:23 +0900

    Created detail pages

--- src/app/products.ts
+++ src/app/products.ts
@@ -2,26 +2,53 @@ export const products = [
   {
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    img:'../../assets/image/iMacimg.png',
+    detail_img:'../../assets/image/iMac.png',
+    heading1:'sample text1',
+    heading2:'sample text2',
+    heading3:'sample text3',
   },
   {
-    name: 'Phone Mini',
+    name: 'iPhone 12 Pro',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    img:'../../assets/image/appstore.png',
+
+    detail_img:'../../assets/image/iPhone12.png',
+    heading1:'sample text1',
+    heading2:'sample text2',
+    heading3:'sample text3',
+    heading_text1:'sampletexttext1',
+    heading_text2:'sampletexttext2',
+    heading_text3:'sampletexttext3',
+
   },
   {
     name: 'Phone Standard',
     price: 299,
-    description: ''
+    description: '',
+    img:'../../assets/image/appstore.png',
+    detail_img:'../../assets/image/iPhone12.png',
+    heading1:'sample text1',
+    heading2:'sample text2',
+    heading3:'sample text3',
+    heading_text1:'sampletexttext1',
+    heading_text2:'sampletexttext2',
+    heading_text3:'sampletexttext3',
   },
   {
     name: 'i Mac - pink',
     price: 1100,
-    description: 'A great PC with one of the best cameras'
+    description: 'A great PC with one of the best cameras',
+    img:'../../assets/image/appstore.png',
+    detail_img:'../../assets/image/iMac.png',
+    heading1:'sample text1',
+    heading2:'sample text2',
+    heading3:'sample text3',
+    heading_text1:'sampletexttext1',
+    heading_text2:'sampletexttext2',
+    heading_text3:'sampletexttext3',
+
   },
-  {
-    name: 'Pixel 10.0',
-    price: 400,
-    description: ''
-  }
 ];
